2dc35dfd70f7b7f3100f86ecc78d80053b3e672b
CleoCTech <cleophasomwenga292@gmail.com>
2023-05-24 00:35:48 +0300

    lesson02 --end

---
+++ lesson02/build/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let myName = 'John';
+let isLoading;
+let album;
+let transactionId;
+let isActive; //true | 1 | yes
+myName = 'Ken';
+isLoading = true;
+album = 1994;
+const sum = (a, b) => {
+    return a + b;
+};
+let re = /\w+/g;
---
+++ lesson02/src/main.ts
@@ -0,0 +1,15 @@
+let myName: string = 'John';
+let isLoading: boolean;
+let album: any;
+let transactionId: string | number;
+let isActive: boolean | number | string; //true | 1 | yes
+
+myName = 'Ken'
+isLoading = true
+album =1994
+
+const sum = (a: number, b: string) => {
+    return a + b
+}
+
+let re: RegExp = /\w+/g
